80f13b490550983f9cd571a46a361581a45a7b74
Alessandro Vioni <alessandro.vioni@objectway.it>
2015-06-22 12:45:27 +0200

    Added tar-gz distribution after dist task

--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,11 @@ gulp.task('clean', env.loadTask('clean'));
 gulp.task('minify', ['move'], env.loadTask('minify'));
 gulp.task('move', env.loadTask('move'));
 gulp.task('copy', env.loadTask('copy-assets'));
-gulp.task('compress', env.loadTask('compress'));
+gulp.task('compress', ['minify'], env.loadTask('compress'));
 
 
 // USEFUL TASKS
-gulp.task('dist', ['clean', 'minify']);
+gulp.task('dist', ['clean', 'compress']);
 gulp.task('compile', ['style', 'view', 'script']);
 gulp.task('default', ['compile', 'srcWatch', 'copy'], env.loadTask('browser-sync'));
 gulp.task('serve', ['srcWatch'], env.loadTask('browser-sync'));
