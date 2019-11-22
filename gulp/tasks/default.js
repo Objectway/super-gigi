9df3edc38c7aca90f99b718542a39405794f699e
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-11-22 19:24:55 +0100

    adds dart-sass support, removes old stuff

--- gulp/tasks/default.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
- module.exports = ['browser-sync'], function () {
-  gulp.watch('./src/assets/sass/**/*.sass', ['sass']);
-  gulp.watch('./src/assets/jade/**/*.jade', ['jade']);
-  gulp.watch('./src/assets/ts/**/*.ts', ['typescript']);
-};
